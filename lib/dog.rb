94e264cded017e03b451d043e8f0155f5aef7cef
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-10-05 04:42:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,27 +28,6 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def Dog::new_from_db(row)
-    # create a new dog object given a row from the database
-    dog = self.new(row[0], row[1], row[2])
-    dog
-  end
-
-  def Dog::find_by_name(name)
-    # find the dog in the database given a name
-    # return a new instance of the dog class
-    sql = <<-SQL
-      SELECT *
-      FROM dogs
-      WHERE name = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
   def save
     if self.id
       self.update
@@ -75,7 +54,29 @@ class Dog
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(result[0], result[1], result[2])
   end
+
   
+  def Dog::new_from_db(row)
+    # create a new dog object given a row from the database
+    dog = self.new(row[0], row[1], row[2])
+    dog
+  end
+
+  def Dog::find_by_name(name)
+    # find the dog in the database given a name
+    # return a new instance of the dog class
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
